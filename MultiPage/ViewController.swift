015e07764c1d6d7a66115b9480eb3899d61b3221
JuyelRana <mjuyelrana@gmail.com>
2019-04-19 07:21:34 +0600

    Initial Commit

---
+++ MultiPage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiPage
+//
+//  Created by Juyel Rana on 4/19/19.
+//  Copyright © 2019 Juyel Rana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
